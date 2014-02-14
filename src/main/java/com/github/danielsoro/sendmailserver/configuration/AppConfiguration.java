00ce737e9aa1e7219e19aa48c4ece685037aa2ec
Daniel Cunha <danielsoro@gmail.com>
2014-02-14 19:26:22 -0300

    Configuration rest in project

---
+++ src/main/java/com/github/danielsoro/sendmailserver/configuration/AppConfiguration.java
@@ -0,0 +1,13 @@
+package com.github.danielsoro.sendmailserver.configuration;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Daniel Cunha (danielsoro@gmail.com)
+ * 
+ */
+@ApplicationPath("/")
+public class AppConfiguration extends Application {
+
+}
---
+++ src/main/java/com/github/danielsoro/sendmailserver/configuration/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Daniel Cunha (danielsoro@gmail.com)
+ *
+ */
+package com.github.danielsoro.sendmailserver.configuration;
