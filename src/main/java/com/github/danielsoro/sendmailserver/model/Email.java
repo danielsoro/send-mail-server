69644df81522579fa17fbf9bcf77252629f76098
Daniel Cunha <danielsoro@gmail.com>
2014-02-14 13:02:56 -0300

    Create Email model

---
+++ src/main/java/com/github/danielsoro/sendmailserver/model/Email.java
@@ -0,0 +1,17 @@
+package com.github.danielsoro.sendmailserver.model;
+
+/**
+ * @author Daniel Cunha (danielsoro@gmail.com)
+ * 
+ */
+public class Email {
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
