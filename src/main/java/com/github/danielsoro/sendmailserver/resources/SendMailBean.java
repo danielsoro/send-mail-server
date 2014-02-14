7f78689b1947562e42a1ff56eaf7ace433e0cb03
Daniel Cunha <danielsoro@gmail.com>
2014-02-14 19:25:46 -0300

    Rename bean, add @RequestScoped and implemetation example method to send mail

--- src/main/java/com/github/danielsoro/sendmailserver/resources/SendMailBean.java
+++ src/main/java/com/github/danielsoro/sendmailserver/resources/SendMailBean.java
@@ -1,24 +1,48 @@
-package com.github.danielsoro.sendmailserver.resources.impl;
+package com.github.danielsoro.sendmailserver.resources;
 
 import javax.annotation.Resource;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.event.Observes;
+import javax.mail.Address;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import com.github.danielsoro.sendmailserver.model.Email;
-import com.github.danielsoro.sendmailserver.resources.SendMsg;
 
 /**
  * @author Daniel Cunha (danielsoro@gmail.com)
  * 
  */
-public class SendMail implements SendMsg {
+@RequestScoped
+public class SendMailBean {
 
-	@Resource(mappedName = "java:jboss/mail/Gmail")
+	@Resource(mappedName = "java:/mail/gmail")
 	private Session session;
 
 	public void send(@Observes Email email) {
-		// TODO Implementar método.
-		throw new UnsupportedOperationException();
-	}
+		try {
+			MimeMessage msg = new MimeMessage(session);
+			msg.setSubject("TESTE", "UTF-8");
 
+			Address[] internetAdress;
+
+			if (email.getAddress() != null) {
+				internetAdress = new Address[email.getAddress().size()];
+				for (int i = 0; i < email.getAddress().size(); i++) {
+					internetAdress[i] = new InternetAddress(email.getAddress()
+							.get(i));
+				}
+				msg.setRecipients(RecipientType.TO, internetAdress);
+			}
+			msg.setText("GAY!! :-)", "UTF-8");
+			msg.setHeader("Content-Type", "text/html;charset=UTF-8");
+			Transport.send(msg);
+		} catch (MessagingException e) {
+			System.out.println(e);
+		}
+	}
 }
